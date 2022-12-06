0e1ee1ced22260c1672b9909dbc79a0dce72cf88
oksanaevseeva <oksana.evseeva@teampicnic.com>
2022-12-06 16:30:03 +0100

    Day 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-import days.Five;
+import days.Six;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(new Five().getTopCratesOld());
-        System.out.println(new Five().getTopCratesNew());
+        System.out.println(new Six().getFirstMarkerIndex(3));
+        System.out.println(new Six().getFirstMarkerIndex(13));
     }
 }
