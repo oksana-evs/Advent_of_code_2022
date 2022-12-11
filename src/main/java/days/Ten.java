233a4a88cbd6ba23502af06986c0622e4b3f9bdc
oksana_evs <moiseeva.ox@gmail.com>
2022-12-11 18:37:04 +0100

    Day 10 refactored

--- src/main/java/days/Ten.java
+++ src/main/java/days/Ten.java
@@ -9,6 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Ten {
+
+    private int currentCycle = 0;
+    private int currentValue = 1;
+    private int currentSpriteStart = 0;
+    private String row = "";
+
     public int getStrength(List<Integer> cycles) throws IOException {
         ArrayList<String> lines = Utils.getLines("10.12.txt");
         int currentValue = 1;
@@ -19,7 +25,7 @@ public class Ten {
                 int valueToAdd = Integer.parseInt(line.split(" ")[1]);
                 map.put(++currentCycle, currentValue);
                 map.put(++currentCycle, currentValue);
-                currentValue+=valueToAdd;
+                currentValue += valueToAdd;
             } else {
                 map.put(++currentCycle, currentValue);
             }
@@ -27,52 +33,37 @@ public class Ten {
         return map.entrySet()
                 .stream()
                 .filter(t -> cycles.contains(t.getKey()))
-                .mapToInt(t -> t.getKey()*t.getValue())
+                .mapToInt(t -> t.getKey() * t.getValue())
                 .sum();
 
     }
 
     public void draw() throws IOException {
         ArrayList<String> lines = Utils.getLines("10.12.txt");
-        int currentValue = 1;
-        int currentCycle = 0;
-        int currentSpriteStart = 0;
-        String row = "";
-        for (String line: lines) {
+        for (String line : lines) {
+            row = doDraw(currentValue, currentSpriteStart, row);
+            increaseCycle();
             if (line.startsWith("addx")) {
-                if (currentSpriteStart == currentValue || currentSpriteStart + 1 == currentValue || currentSpriteStart + 2 == currentValue) {
-                    row = row + "#";
-                } else {
-                    row = row + ".";
-                }
-                currentCycle++;
-                currentSpriteStart++;
                 int valueToAdd = Integer.parseInt(line.split(" ")[1]);
-                currentValue+=valueToAdd;
-                row = getString(currentValue, currentSpriteStart, row);
-                currentCycle++;
-                currentSpriteStart++;
-            } else {
-                if (currentSpriteStart == currentValue || currentSpriteStart + 1 == currentValue || currentSpriteStart + 2 == currentValue) {
-                    row = row + "#";
-                } else {
-                    row = row + ".";
-                }
-                currentCycle++;
-                currentSpriteStart++;
-            }
-
-            //new line
-            if ((currentCycle) % 40 == 0) {
-                currentSpriteStart = 0;
-                System.out.println(row);
-                row = "";
+                currentValue += valueToAdd;
+                row = doDraw(currentValue, currentSpriteStart, row);
+                increaseCycle();
             }
         }
 
     }
 
-    private static String getString(int currentValue, int currentSpriteStart, String row) {
+    private void increaseCycle() {
+        currentCycle++;
+        currentSpriteStart++;
+        if ((currentCycle) % 40 == 0) {
+            currentSpriteStart = 0;
+            System.out.println(row);
+            row = "";
+        }
+    }
+
+    private static String doDraw(int currentValue, int currentSpriteStart, String row) {
         if (currentSpriteStart == currentValue || currentSpriteStart + 1 == currentValue || currentSpriteStart + 2 == currentValue) {
             row = row + "#";
         } else {
